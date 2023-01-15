9a689fcd01f2eb7dfa36513b61fa51d1df61d011
MaximoFC <maximofcallejas@gmail.com>
2023-01-15 20:57:22 -0300

    nuevas url

--- src/main/java/com/portfolio/mfc/Controller/PersonaController.java
+++ src/main/java/com/portfolio/mfc/Controller/PersonaController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "https://frontendportfoliomfc.web.app/home")
+@CrossOrigin(origins = "https://frontendportfoliomfc-12dd7.web.app/home")
 public class PersonaController {
 
     @Autowired
