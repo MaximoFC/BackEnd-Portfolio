97786910862db74f2b6c001342daca681b45dd77
MaximoFC <maximofcallejas@gmail.com>
2023-01-16 14:25:53 -0300

    corsconfiguration

--- src/main/java/com/portfolio/mfc/MfcApplication.java
+++ src/main/java/com/portfolio/mfc/MfcApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class MfcApplication {
@@ -16,18 +16,12 @@ public class MfcApplication {
 	}
         
         @Bean
-        public CorsFilter corsFilter(){
-            CorsConfiguration corsConfiguration = new CorsConfiguration();         
-            corsConfiguration.setAllowCredentials(true);         
-            corsConfiguration.setAllowedOrigins(Arrays.asList("https://frontendportfoliomfc-12dd7.web.app"));         
-            corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",                 
-                    "Accept", "Authorization", "Origin, Accept", "X-Requested-With",                 
-                    "Access-Control-Request-Method", "Access-Control-Request-Headers"));         
-            corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",                 
-                    "Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));         
-            corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));         
-            UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
-            urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
-            return new CorsFilter(urlBasedCorsConfigurationSource);
+        protected CorsConfigurationSource corsConfigurationSource(){
+            CorsConfiguration configuration = new CorsConfiguration();
+            configuration.setAllowedOrigins(Arrays.asList("*"));
+            configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+            source.registerCorsConfiguration("/**", configuration);
+            return source;
         }
 }
