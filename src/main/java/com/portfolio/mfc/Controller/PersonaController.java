d85b17b791a363c7fdbb0f1e4316857d4035d616
MaximoFC <maximofcallejas@gmail.com>
2023-01-23 16:21:02 -0300

    crossorigin

--- src/main/java/com/portfolio/mfc/Controller/PersonaController.java
+++ src/main/java/com/portfolio/mfc/Controller/PersonaController.java
@@ -4,6 +4,7 @@ import com.portfolio.mfc.Entity.Persona;
 import com.portfolio.mfc.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins ="https://frontendportfoliomfc-12dd7.web.app/home")
 @RestController
 public class PersonaController {
 
