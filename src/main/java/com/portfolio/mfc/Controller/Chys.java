5193d21b3997d60a022524f300ffccab1d55a645
MaximoFC <maximofcallejas@gmail.com>
2023-01-23 15:56:34 -0300

    configuración global de cors

--- src/main/java/com/portfolio/mfc/Controller/Chys.java
+++ src/main/java/com/portfolio/mfc/Controller/Chys.java
@@ -8,7 +8,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "https://frontendportfoliomfc-12dd7.web.app/")
 @RestController
 @RequestMapping("/hys")
 public class Chys {
--- src/main/java/com/portfolio/mfc/WebConfig.java
+++ src/main/java/com/portfolio/mfc/WebConfig.java
@@ -11,10 +11,6 @@ public class WebConfig implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry) {
 
-        registry.addMapping("/**")
-                .allowedOrigins("https://frontendportfoliomfc-12dd7.web.app/")
-                .allowedMethods("*")
-                .allowedHeaders("*");
-
+        registry.addMapping("/**");
     }
 }
