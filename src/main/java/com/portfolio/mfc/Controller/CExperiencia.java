f596335575ec8011d761e1cd20bdbcfd154b37ff
MaximoFC <maximofcallejas@gmail.com>
2023-01-25 12:01:40 -0300

    quito los crossorigin del controller

--- src/main/java/com/portfolio/mfc/Controller/CExperiencia.java
+++ src/main/java/com/portfolio/mfc/Controller/CExperiencia.java
@@ -8,7 +8,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins="https://frontendportfoliomfc-12dd7.web.app")
 @RestController
 @RequestMapping("/experiencia")
 public class CExperiencia {
